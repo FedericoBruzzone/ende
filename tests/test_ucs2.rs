fef2b1a87ae98b69a2f7398984d0b31d08d84dc9
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2024-04-14 22:41:06 +0200

    Add tests
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

---
+++ tests/test_ucs2.rs
@@ -0,0 +1,29 @@
+use encdec::prelude::*;
+
+#[test]
+fn test_ucs2_encode_in_ucs2_1() {
+    let v: Vec<u32> = vec![0xffee];
+    let enc: Vec<u16> = encode_in_ucs2(v);
+    assert_eq!(enc, vec![0xffee]);
+}
+
+#[test]
+fn test_ucs2_decode_from_ucs2_1() {
+    let enc: Vec<u16> = vec![0xffee];
+    let v: Vec<u32> = decode_from_ucs2(enc);
+    assert_eq!(v, vec![0xffee]);
+}
+
+#[test]
+fn test_ucs2_encode_in_ucs2_2() {
+    let v: Vec<u32> = vec![0xffee, 0xffee];
+    let enc: Vec<u16> = encode_in_ucs2(v);
+    assert_eq!(enc, vec![0xffee, 0xffee]);
+}
+
+#[test]
+fn test_ucs2_decode_from_ucs2_2() {
+    let enc: Vec<u16> = vec![0xffee, 0xffee];
+    let v: Vec<u32> = decode_from_ucs2(enc);
+    assert_eq!(v, vec![0xffee, 0xffee]);
+}
---
+++ tests/test_utf16.rs
@@ -0,0 +1,57 @@
+use encdec::prelude::*;
+
+#[test]
+fn test_utf16_encode_in_utf16_1() {
+    let v: Vec<u32> = vec![0x10001];
+    let enc: Vec<u16> = encode_in_utf16(v);
+    assert_eq!(enc, vec![0xd800, 0xdc01]);
+}
+
+#[test]
+fn test_utf16_decode_from_utf16_1() {
+    let enc: Vec<u16> = vec![0xd800, 0xdc01];
+    let v: Vec<u32> = decode_from_utf16(enc);
+    assert_eq!(v, vec![0x10001]);
+}
+
+#[test]
+fn test_utf16_encode_in_utf16_2() {
+    let v: Vec<u32> = vec![0x10001, 0x10001];
+    let enc: Vec<u16> = encode_in_utf16(v);
+    assert_eq!(enc, vec![0xd800, 0xdc01, 0xd800, 0xdc01]);
+}
+
+#[test]
+fn test_utf16_decode_from_utf16_2() {
+    let enc: Vec<u16> = vec![0xd800, 0xdc01, 0xd800, 0xdc01];
+    let v: Vec<u32> = decode_from_utf16(enc);
+    assert_eq!(v, vec![0x10001, 0x10001]);
+}
+
+#[test]
+fn test_utf16_encode_in_utf16_3() {
+    let v: Vec<u32> = vec![0x10001, 0x10001, 0x10001];
+    let enc: Vec<u16> = encode_in_utf16(v);
+    assert_eq!(enc, vec![0xd800, 0xdc01, 0xd800, 0xdc01, 0xd800, 0xdc01]);
+}
+
+#[test]
+fn test_utf16_decode_from_utf16_3() {
+    let enc: Vec<u16> = vec![0xd800, 0xdc01, 0xd800, 0xdc01, 0xd800, 0xdc01];
+    let v: Vec<u32> = decode_from_utf16(enc);
+    assert_eq!(v, vec![0x10001, 0x10001, 0x10001]);
+}
+
+#[test]
+fn test_utf16_encode_in_utf16_4() {
+    let v: Vec<u32> = vec![0x10001, 0x23456];
+    let enc: Vec<u16> = encode_in_utf16(v);
+    assert_eq!(enc, vec![0xd800, 0xdc01, 0xd84d, 0xdc56]);
+}
+
+#[test]
+fn test_utf16_decode_from_utf16_4() {
+    let enc: Vec<u16> = vec![0xd800, 0xdc01, 0xd84d, 0xdc56];
+    let v: Vec<u32> = decode_from_utf16(enc);
+    assert_eq!(v, vec![0x10001, 0x23456]);
+}
